23464169823d8275ed4ba31e55306ca4923b3c39
PythonCoderAS <sarkaraoyan@gmail.com>
2022-05-15 09:31:04 -0400

    Create stub file

---
+++ js/src/internal.ts
@@ -0,0 +1,6 @@
+/**
+ * This module contains types that do not exist in the Python version of the documentation.
+ * Unlike in sphinx, it is not possible to create documentation from non-code source files.
+ * @todo If this file is not needed, delete the file. Otherwise, delete this todo.
+ * @module
+ */
