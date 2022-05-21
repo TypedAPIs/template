c18e295c9bd51648415255228490ed24412e46df
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-21 06:34:21 -0400

    Copy from guyamoe repo

--- scripts/populate-test-data.sh
+++ scripts/populate-test-data.sh
@@ -12,7 +12,7 @@ mkdir -p "$JS_DATA_DIR"
 
 function jsonToPython() {
     # Usage: jsonToPython <json file> <python file>
-    python3 -c $'import json, sys\nprint("data = w" + repr(json.loads(sys.stdin.read())))' < "$1" > "$2"
+    python3 -c $'import json, sys\nprint("data = " + repr(json.loads(sys.stdin.read())))' < "$1" > "$2"
 }
 
 PY_DATA_DIR="$BASEDIR/python/tests/examples"
